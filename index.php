790187dc812f3749fd6cb242f57cc0459b32f9f5
Puskar <puskarbha@gmail.com>
2022-05-30 12:42:27 +0545

    Email checkpoint added successfully.

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php
 //scrip to conect to the database
 $servername ="localhost";
@@ -22,34 +21,46 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
    $name=$_POST['f_name'];
    $password=$_POST['f_pass'];
   $cpassword=$_POST['f_cpass'];
-  //Firstly check whether two passwords matches
-  if($password==$cpassword)
-  {
-            //converting password into hash form
-        $hashed_pass = password_hash($password,PASSWORD_DEFAULT);
-        $sql = "INSERT INTO `users` (`user_email`, `user_name`, `user_password`, `timestamp`) VALUES ( '$email', '$name', '$hashed_pass', current_timestamp())";
-        $result = mysqli_query($conn,$sql);
-        if($result){
-          echo"<h1>Signup successfully.</h1>";
-       
-        exit();
+
+  $sql1="SELECT * FROM `users` WHERE user_email = '$email'";
+  
+  $result1 = mysqli_query($conn,$sql1);
+  $numRows = mysqli_num_rows($result1);
+  if($numRows>0){
+    echo"<h1>Error!</h1><h2>Email already exists</h2>";
+    exit();
+  }
+  else{
+ //Firstly check whether two passwords matches
+            if($password==$cpassword)
+            {
+                      //converting password into hash form
+                  $hashed_pass = password_hash($password,PASSWORD_DEFAULT);
+                  $sql = "INSERT INTO `users` (`user_email`, `user_name`, `user_password`, `timestamp`) VALUES ( '$email', '$name', '$hashed_pass', current_timestamp())";
+                  $result = mysqli_query($conn,$sql);
+                  if($result){
+                    echo"<h1>Signup successfully.</h1>";
+                
+                  exit();
+                  }
+            }
+            else{
+            echo"<h1>password doesnot match</h1>";
+            exit();
+            }
         }
-}
-else{
-  echo"<h1>password doesnot match</h1>";
-  exit();
-}
+ 
   }
    
 ?>
-<form   method="post">
+<form method="post">
     <b><u>Please sighup to continue:</u></br></br>
         <b>Email address:</b>
         <input id="f_email" name="f_email" type="email"></br></br>
         <b>Full name:</b>
         <input id="f_name" name="f_name"></br></br>
         <b>Password:</b>
-        <input id="f_pass" name="f_pass" type="password" ></br></br>
+        <input id="f_pass" name="f_pass" type="password"></br></br>
         <b>Confirm Password:</b>
         <input id="f_cpass" name="f_cpass" type="password"></br></br>
         <button>signup</button>
